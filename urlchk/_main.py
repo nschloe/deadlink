aca6f6dc2b00200f8d622b917d350b85a833fd63
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-20 11:45:39 +0200

    scan directories

--- urlchk/_main.py
+++ urlchk/_main.py
@@ -4,21 +4,34 @@ from pathlib import Path
 import requests
 from rich.progress import track
 
+# https://regexr.com/3e6m0
+# make all groups non-capturing with ?:
+pattern = re.compile(
+    r"http(?:s)?:\/\/.(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+)
+
+
+def _get_urls_from_file(path):
+    try:
+        with open(path) as f:
+            content = f.read()
+    except UnicodeDecodeError:
+        return []
+    return pattern.findall(content)
+
 
 def check(path):
     path = Path(path)
 
-    # https://regexr.com/3e6m0
-    # make all groups non-capturing with ?:
-    pattern = re.compile(
-        r"http(?:s)?:\/\/.(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)"
-    )
-
     matches = []
-    if path.is_file():
-        with open(path) as f:
-            fread = f.read()
-            matches += pattern.findall(fread)
+    if path.is_dir():
+        for p in path.rglob("*"):
+            if p.is_file():
+                matches += _get_urls_from_file(p)
+    elif not path.is_file():
+        matches += _get_urls_from_file(path)
+    else:
+        raise ValueError(f"Could not find path {path}.")
 
     print(f"Found {len(matches)} URLs")
     not_ok = []
