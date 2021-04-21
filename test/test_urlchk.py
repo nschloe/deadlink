638dea5174c957a94fe62122ba65360254ecc6ba
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-21 08:49:28 +0200

    update some urls

--- test/test_urlchk.py
+++ test/test_urlchk.py
@@ -2,5 +2,5 @@ import urlchk
 
 
 def test_check():
-    has_errors = urlchk.check_urls({"https://google.com"})
+    has_errors = urlchk.check_urls({"https://www.google.com"})
     assert not has_errors
