7d3b2c1ffca48dacfbb772609dbab18abbbe54dd
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-21 09:08:47 +0200

    more coverage

--- test/test_urlchk.py
+++ test/test_urlchk.py
@@ -5,5 +5,13 @@ def test_check():
     has_errors = urlchk.check_urls({"https://www.google.com"})
     assert not has_errors
 
+    has_errors = urlchk.check_urls({"https://github.com/nschloe/urlchk/void"})
+    assert has_errors
+
+    # "other errors"
     has_errors = urlchk.check_urls({"https://this-doesnt-exist.doesit"})
     assert has_errors
+
+    # redirect
+    has_errors = urlchk.check_urls({"https://pypi.org/pypi/pygalmesh/"})
+    assert not has_errors
