4b8cf08680f6e2003c1c9dd223ae412e966f5dcf
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-21 08:54:59 +0200

    add failing test

--- test/test_urlchk.py
+++ test/test_urlchk.py
@@ -4,3 +4,6 @@ import urlchk
 def test_check():
     has_errors = urlchk.check_urls({"https://www.google.com"})
     assert not has_errors
+
+    has_errors = urlchk.check_urls({"https://this-doesnt-exist.doesit"})
+    assert has_errors
